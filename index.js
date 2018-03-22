03b7a906b964611bb092a6081e02a6ba148a1843
Andre <drinkins85@mail.ru>
2018-03-23 02:11:22 +0300

    add git api

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const app = express();
 const port = process.env.PORT || 3003;
 const host = '0.0.0.0';
 
+const getBtanchList = require('./gitAPI/getBranchList');
+
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
   res.send('Hello, World');
 });
+
+app.get('/git', (req, res) => {
+  getBtanchList().then((branches) => {
+    res.send(`Hello, Git ${branches}`);
+  });
+});
+
 
 app.listen(port, host);
 
